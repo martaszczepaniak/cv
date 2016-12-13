6a0df1c09d45482b293d1c92f0a3524af00b552f
Marta Szczepaniak <mart.szcz@gmail.com>
2016-12-13 21:17:51 +0100

    Add Page Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Page from './Page'
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <Page>
+      </Page>
     );
   }
 }
---
+++ src/Page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Page(props) {
+  return <div className="a4">{props.children}</div>;
+}
+
+export default Page;
